4a92b55271aa4fa2e83ec9e9098117ed573ceb86
Carlos Netto <cnetto@yahoo.com>
2026-02-11 20:03:03 -0600

    Fix: keep stdin open in launch script

--- launch_40.sh
+++ launch_40.sh
@@ -10,7 +10,7 @@ do
    # Launch in background, redirecting input from a pipe to keep them alive
    # and output to a log file.
    echo "Starting instance $i..."
-   sleep 0.1 | java BigProgram > "instance_$i.log" 2>&1 &
+   tail -f /dev/null | java BigProgram > "instance_$i.log" 2>&1 &
 done
 
 echo "40 instances launched. Use 'jps' or 'ps' to see them."
